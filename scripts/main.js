64e3ef4fee5975f0b861ae602c55b3aa9e41a7cd
Jose <estaravia@hotmail.com>
2020-03-09 20:22:06 -0500

    input interaction correction

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
-function changeColor() {
-        
-        console.log('asdas');
+
+function red() {
+        document.getElementById('product').src= "./images/red switch.png";
+    }
+function blue() {
+    document.getElementById('product').src= "./images/blue switch.png";
+    }
+function orange() {
+    document.getElementById('product').src= "./images/yellow switch.png";
     }
